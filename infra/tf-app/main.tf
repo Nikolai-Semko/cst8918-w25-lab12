2c0d7feddf51cc3a1ac3e0ac89f677ac971dcb0e
NikolaiSemko <semk0001@algonquinlive.com>
2025-07-24 01:18:18 -0400

    going back a step

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,17 +2,3 @@ resource "azurerm_resource_group" "main" {
   name     = "semk0001-a12-rg"
   location = "East US"
 }
-
-resource "azurerm_virtual_network" "main" {
-  name                = "vnet-main"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "main" {
-  name                 = "subnet-main"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
--- infra/tf-app/outputs.tf
+++ infra/tf-app/outputs.tf
@@ -1,11 +1,3 @@
 output "resource_group_name" {
   value = azurerm_resource_group.main.name
 }
-
-output "virtual_network_name" {
-  value = azurerm_virtual_network.main.name
-}
-
-output "subnet_name" {
-  value = azurerm_subnet.main.name
-}
